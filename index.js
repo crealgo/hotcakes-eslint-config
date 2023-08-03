6b03d47b97e083e76b761556a78900cc3d0b1910
Manas Chimpidi <manaschimpidi@gmail.com>
2023-08-02 23:41:34 -0500

    init

---
+++ index.js
@@ -0,0 +1,48 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+		es6: true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'xo',
+		'xo-typescript',
+		'xo-react',
+	],
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'react/prop-types': 'off',
+		'react/function-component-definition': [
+			'error',
+			{
+				namedComponents: 'arrow-function',
+			},
+		],
+		'react/no-array-index-key': 'off',
+		'react/require-default-props': 'off',
+		'implicit-arrow-linebreak': ['error', 'beside'],
+		'no-warning-comments': 'off',
+		'no-useless-rename': 'error',
+		'no-useless-return': 'error',
+		'@typescript-eslint/ban-types': 'off',
+		'@typescript-eslint/consistent-type-assertions': 'off',
+		'@typescript-eslint/naming-convention': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-confusing-void-expression': 'off',
+		'@typescript-eslint/await-thenable': 'off',
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/object-shorthand': 'off',
+		'@typescript-eslint/capitalized-comments': 'off',
+		'@typescript-eslint/guard-for-in': 'off',
+	}
+};
