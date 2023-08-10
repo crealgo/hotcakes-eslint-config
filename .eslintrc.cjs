192abd3150c44c5d1eee3a6df79bc45b7e84d54d
Manas Chimpidi <manaschimpidi@gmail.com>
2023-08-10 01:25:25 -0500

    simplify

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,22 +1,4 @@
 require('@rushstack/eslint-patch/modern-module-resolution');
 
 /** @type {import('eslint').ESLint.ConfigData} */
-module.exports = {
-	extends: [
-		'xo',
-		'xo-react',
-	],
-	rules: {
-		'react/function-component-definition': [2, {
-			namedComponents: 'arrow-function',
-			unnamedComponents: 'arrow-function',
-		}],
-		'react/react-in-jsx-scope': 'off',
-	},
-	overrides: [
-		{
-			files: ['*.tsx', '*.ts'],
-			extends: 'xo-typescript',
-		},
-	],
-};
+module.exports = require('./index.js');
